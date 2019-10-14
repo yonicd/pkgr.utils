5b1891adcf5e8fe0e063033c4c5cfecf8d9109db
Jonathan Sidi <yonis@metrumrg.com>
2019-10-14 09:51:11 -0400

    refactor: update get_repos to include snapshot dates docs: bump version

--- R/repos.R
+++ R/repos.R
@@ -1,19 +1,41 @@
 #' @title Stored Repositories
 #' @description Returns names of stored CRAN and CRANlike repositories
 #' @param name character, Names of repositories to return,
-#'    Default: c("gh_external", "gh_dev", "mrg_val", "rs_cran")
+#'    Default: c("MPN", "CRAN")
+#' @param snapshot Date, date of the snapshot of the repository, Default: Sys.Date()
+#' @details
+#'
+#' Snapshot value is coerced into a Date class internally.
+#'
+#' The earliest date for 'MPN' is '2019-09-22', if snapshot is earlier than this and
+#' the user includes 'MPN' in name then snapshot will be replaced with '2019-09-22'.
+#'
 #' @return character
 #' @examples
 #' get_repos()
+#'
+#' get_repos(name = 'MPN')
+#'
+#' get_repos(name = 'MPN', snapshot = Sys.Date() - 1)
+#'
 #' @rdname get_repos
 #' @export
-get_repos <- function(name = c('gh_external','gh_dev','mrg_val','rs_cran')){
+#' @importFrom glue glue
+get_repos <- function(name = c('MPN','CRAN'), snapshot = Sys.Date()){
 
-  repos <- c(gh_external = 'https://metrumresearchgroup.github.io/rpkgs/gh_external',
-    gh_dev      = 'https://metrumresearchgroup.github.io/rpkgs/gh_dev',
-    mrg_val     = 'https://metrumresearchgroup.github.io/r_validated',
-    rs_cran = 'https://cran.rstudio.com'
-  )
+  min_date <- as.Date('2019-09-22')
+
+  snapshot <- as.Date(snapshot)
+
+  if('MPN' %in% name & snapshot < min_date){
+    warning('MPN starts at "2019-09-22", resetting snapshot to that date')
+    snapshot <- min_date
+  }
+
+  repos <-   c(
+    MPN =  glue::glue('https://metrumresearchgroup.github.io/cran/{snapshot}'),
+    CRAN = glue::glue('https://cran.microsoft.com/snapshot/{snapshot}')
+    )
 
   repos[match.arg(name,names(repos),several.ok = TRUE)]
 
